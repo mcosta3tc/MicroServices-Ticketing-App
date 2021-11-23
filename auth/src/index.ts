eb70ddad4292068d139b9ca5086b679345723c7c
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-11-23 10:11:17 +0100

    gcloud added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import {json} from 'express'
+import { currentUserRouter } from './routes/currentUser';
 
 const app = express();
 app.use(json());
 
+//Current User Route
+app.use(currentUserRouter)
+
 app.listen(3000, () => {
-    console.log('Listening on : 3000 !');
+    console.log('Listening on : 3000!');
 })
