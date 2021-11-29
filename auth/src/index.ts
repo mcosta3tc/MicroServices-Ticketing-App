3cf22199472b5cab114e2d8f969784aa3db67cf7
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-11-29 16:43:56 +0100

    mongo init

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,17 @@
+import mongoose from "mongoose";
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Listening on : 3000!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on : 3000!");
+  });
+};
+
+start();
